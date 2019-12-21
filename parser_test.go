1e5f07bec3d56cb77e9a3b56725da170a9ec5960
Akash Suresh <akash@signalfx.com>
2019-12-21 13:51:08 -0500

    Improve tests

--- parser_test.go
+++ parser_test.go
@@ -11,7 +11,7 @@ func TestProcessLogs(t *testing.T) {
 		// router error log
 		"277 <158>1 2012-10-11T03:47:20+00:00 host heroku router - at=error code=H12 desc=\"Request timeout\" method=GET path=/ host=myapp.herokuapp.com request_id=8601b555-6a83-4c12-8269-97c8e32cdb22 fwd=\"204.204.204.204\" dyno=web.1 connect= service=30000ms status=503 bytes=0 protocol=http",
 		// router info log
-		"271 <158>1 2019-12-11T16:17:53.786555+00:00 host heroku router - at=info method=GET path=\"/test\" host=aqueous-oasis-14017.herokuapp.com request_id=93bf8b6c-34b1-4eb8-9b5b-f0e72e5ce377 code=H12 fwd=\"76.195.93.225\" dyno=web.1 connect=0ms service=1ms status=404 bytes=146 protocol=https",
+		"271 <158>1 2019-12-11T16:17:53.786555+00:00 host heroku router - at=info method=GET path=\"/test\" host=aqueous-oasis-14017.herokuapp.com request_id=93bf8b6c-34b1-4eb8-9b5b-f0e72e5ce377 fwd=\"76.195.93.225\" dyno=web.1 connect=0ms service=1ms status=404 bytes=146 protocol=https",
 		// dyno metricVal logs
 		"277 <45>1 2019-12-11T22:29:21.372436+00:00 host heroku web.1 - source=web.1 dyno=heroku.155370883.259625dd-a9c7-4987-9c86-08de28dd4f72 sample#memory_total=99.74MB sample#memory_rss=97.91MB sample#memory_cache=1.83MB sample#memory_swap=0.00MB sample#memory_pgpgin=355603pages sample#memory_pgpgout=333646pages sample#memory_quota=512.00MB",
 		"277 <45>1 2019-12-11T22:29:21.372436+00:00 host heroku web.2 - source=web.2 dyno=heroku.155370883.e764d0ed-b239-4048-9caa-38a78dfeb6d0 sample#load_avg_1m=0.00",
@@ -33,7 +33,7 @@ func TestProcessLogs(t *testing.T) {
 			Hostname:  "host",
 			Appname:   "heroku",
 			ProcId:    "router",
-			Message:   "at=info method=GET path=\"/test\" host=aqueous-oasis-14017.herokuapp.com request_id=93bf8b6c-34b1-4eb8-9b5b-f0e72e5ce377 code=H12 fwd=\"76.195.93.225\" dyno=web.1 connect=0ms service=1ms status=404 bytes=146 protocol=https",
+			Message:   "at=info method=GET path=\"/test\" host=aqueous-oasis-14017.herokuapp.com request_id=93bf8b6c-34b1-4eb8-9b5b-f0e72e5ce377 fwd=\"76.195.93.225\" dyno=web.1 connect=0ms service=1ms status=404 bytes=146 protocol=https",
 		}, {
 			PRI:       "45",
 			Version:   "1",
@@ -54,6 +54,7 @@ func TestProcessLogs(t *testing.T) {
 	}
 
 	numExpectedMetrics := []int{2, 3, 7, 1}
+	numExpectedDimensions := []int{8, 7, 5, 5}
 
 	for i, input := range validInputs {
 		actual, _ := detectAndParseLog(input)
@@ -64,14 +65,19 @@ func TestProcessLogs(t *testing.T) {
 			t.Error("Parsed log output does not match expected")
 		}
 
-		metrics, _ := processMetrics(actual, map[string]string{
+		metrics, dims := processMetrics(actual, map[string]string{
 			"app_name": "test-app",
 		})
 
 		if numExpectedMetrics[i] != len(metrics) {
-			t.Errorf("Expected %d datapoints, received %d datapoints", numExpectedMetrics[i], len(metrics))
+			t.Logf("Actual: %v", metrics)
+			t.Errorf("Expected %d metrics, received %d metrics", numExpectedMetrics[i], len(metrics))
 		}
 
+		if numExpectedDimensions[i] != len(dims) {
+			t.Logf("Actual: %s", dims)
+			t.Errorf("Expected %d dimensions, received %d dimensions", numExpectedDimensions[i], len(dims))
+		}
 	}
 
 }
--- registry_test.go
+++ registry_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"testing"
 	"time"
+
+	"github.com/signalfx/golib/v3/datapoint"
 )
 
 func setTime(r *metricRegistry, t time.Time) {
@@ -18,10 +20,10 @@ func TestExpiration(t *testing.T) {
 
 	setTime(mr, time.Unix(100, 0))
 
-	mr.registerOrGetCumulative("test", map[string]string{"a": "1"})
-	mr.registerOrGetCumulative("test", map[string]string{"a": "2"})
-	mr.registerOrGetCumulative("test", map[string]string{"a": "3"})
-	mr.registerOrGetCumulative("test", map[string]string{"a": "4"})
+	mr.registerOrGetCumulative("test1", map[string]string{"a": "1"})
+	mr.registerOrGetCumulative("test1", map[string]string{"a": "2"})
+	mr.registerOrGetGauge("test2", map[string]string{"a": "1"}, datapoint.Gauge)
+	mr.registerOrGetGauge("test2", map[string]string{"a": "2"}, datapoint.Gauge)
 
 	mr.Datapoints()
 	if len(mr.Datapoints()) != 4 {
@@ -35,11 +37,11 @@ func TestExpiration(t *testing.T) {
 		t.Fatalf("Expected four datapoints")
 	}
 
-	mr.registerOrGetCumulative("test", map[string]string{"a": "4"})
-	mr.registerOrGetCumulative("test", map[string]string{"a": "5"})
+	mr.registerOrGetCumulative("test1", map[string]string{"a": "3"})
+	mr.registerOrGetGauge("test2", map[string]string{"a": "3"}, datapoint.Gauge)
 
 	mr.Datapoints()
-	if len(mr.Datapoints()) != 5 {
+	if len(mr.Datapoints()) != 6 {
 		t.Fatalf("Expected five datapoints")
 	}
 
@@ -48,11 +50,11 @@ func TestExpiration(t *testing.T) {
 	mr.Datapoints()
 	dps := mr.Datapoints()
 	if len(dps) != 2 {
-		t.Fatalf("Expected first three counters to be expired")
+		t.Fatalf("Expected first four counters to be expired")
 	}
 
 	for _, dp := range dps {
-		if dp.Dimensions["a"] == "1" || dp.Dimensions["a"] == "2" || dp.Dimensions["a"] == "3" {
+		if !(dp.Metric == "test1" || dp.Metric == "test2" || dp.Dimensions["a"] == "3") {
 			t.Fatalf("Wrong counter was expired")
 		}
 	}
